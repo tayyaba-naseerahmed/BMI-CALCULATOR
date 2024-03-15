b46b2613fed8f3bb2875e5c729071508c8943ce8
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-15 06:26:55 +0500

    BMI CALCULATOR

---
+++ main.js
@@ -0,0 +1,4 @@
+var weightInKg = 60;
+var heightInMeters = 1.60;
+var bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log("Your BMI is ".concat(bmi));
---
+++ main.ts
@@ -0,0 +1,5 @@
+let weightInKg = 60; 
+let heightInMeters = 1.60;
+let bmi = weightInKg / (heightInMeters * heightInMeters) 
+
+console.log(`Your BMI is ${bmi}`)
